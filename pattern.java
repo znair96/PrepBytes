798131fd4e7cc709f313310cec321b50da37e7bb
Zubin Nair <43044753+znair96@users.noreply.github.com>
2019-10-20 23:43:32 +0530

    Pattern

---
+++ pattern.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Pattern {
+    public static void main(String[] args)
+    {
+         Scanner s = new Scanner(System.in);
+         int row = s.nextInt();
+         int  column = 2*row-1;
+         int mid = (column+1)/2;
+         for(int i=1;i<=row;i++)
+         {
+             int count=0;
+             for(int j=1;j<=column;j++)
+             {
+                 if(j>=((mid+1)-i)&&j<=((mid-1)+i))
+                 {
+                     count++;
+                     if(count%2!=0)
+                     {
+                         System.out.print("*");
+                     }
+                     else
+                     {
+                         System.out.print(" ");
+                     }
+                 }
+                 else
+                 {
+                     System.out.print(" ");
+                 }
+             }
+             System.out.println();
+         }
+    }
+}
